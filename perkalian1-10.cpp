f855d26f1381cad7a0ee40087ab7c8deda61a7b1
WunCah <56233158+WunCah@users.noreply.github.com>
2019-10-07 22:31:48 +0700

    Add files via upload

---
+++ perkalian1-10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Nur Wulan Cahyani \n";
+	cout<<"19051397010 \n";
+	cout<<"D4 Manajemen Informatika \n";
+	cout<<"Selamat Datang Di Perkalian 1-10 \n";
+
+	int x,nilai,total,a;
+    cout<<"mau sampai berapa perkaliannya?";cin>>nilai;
+    cout<<"masukkan bilangan yan mau dikalikan:";cin>>a;
+    cout<<"\n hasil perkaliannya :\n";
+    for(x=0;x<=nilai;x++)
+    {total=x*a;
+    cout<<x<<"*"<<a<<"="<<total<<endl;}
+    {
+     return 0;
+    }
+}
